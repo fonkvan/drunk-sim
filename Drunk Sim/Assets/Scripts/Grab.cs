e696de2ea893a721b6c0d934954b6f1013c042e5
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-04-07 17:02:28 -0400

    Completed Grab script
    Now player character extends arms to grab, rotates hands to face object, and object snaps to NEAR player hand when grabbed. Pressing key again will drop object from respective hand. Need to clean up snapping so it isn't floating near player but attached to hand itself.

--- Drunk Sim/Assets/Scripts/Grab.cs
+++ Drunk Sim/Assets/Scripts/Grab.cs
@@ -6,39 +6,126 @@ public class Grab : MonoBehaviour
 {
     private GameObject grabbedObject;
     private Rigidbody rb;
-    public bool alreadyGrabbed = false;
+    public ConfigurableJoint armToRotate;
+    private ConfigurableJoint thisHand;
+
+    public bool leftAlreadyGrabbed = false;
+    public bool rightAlreadyGrabbed = false;
+    public int isLeftOrRight;
+
+    private Quaternion naturalArmRotation = Quaternion.Euler(310f, 0f, 0f);
+    private Quaternion leftGrabRotation = Quaternion.Euler(10f, 310f, 60f);
+    private Quaternion rightGrabRotation = Quaternion.Euler(350f, 50f, 300f);
+
+    private Quaternion naturalHandRotation = Quaternion.identity;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        thisHand = GetComponent<ConfigurableJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if (isLeftOrRight == 1)
         {
-            if (!alreadyGrabbed)
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                print("trying to grab");
-                FixedJoint fj = grabbedObject.AddComponent<FixedJoint>();
-                fj.connectedBody = rb;
-                fj.breakForce = 9000;
+                if (!rightAlreadyGrabbed)
+                {
+                    armToRotate.targetRotation = rightGrabRotation;
 
-                print(grabbedObject);
-                print(fj);
+                    print("trying to grab");
 
-                alreadyGrabbed = true;
+                    if (grabbedObject != null)
+                    {
+                        FixedJoint fj = grabbedObject.AddComponent<FixedJoint>();
+                        fj.connectedBody = rb;
+                        fj.breakForce = 9000;
 
+                        thisHand.targetRotation = grabbedObject.transform.rotation;
+                        
+
+                        print(grabbedObject);
+                        print(fj);
+
+                        if (fj.connectedBody == rb)
+                        {
+                            rightAlreadyGrabbed = true;
+                            print("grabbed " + grabbedObject);
+                        }
+                    }
+
+                }
+                else
+                {
+                    Destroy(grabbedObject.GetComponent<FixedJoint>());
+                    print("letting go");
+                    grabbedObject = null;
+
+                    armToRotate.targetRotation = naturalArmRotation;
+
+                    thisHand.targetRotation = naturalHandRotation;
+
+                    if (grabbedObject == null)
+                    {
+                        rightAlreadyGrabbed = false;
+                        print("dropped an object");
+                    }
+                }
             }
-            else
+
+        }
+        else if (isLeftOrRight == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                Destroy(grabbedObject.GetComponent<FixedJoint>());
-                print("letting go");
-                grabbedObject = null;
+                if (!leftAlreadyGrabbed)
+                {
+                    armToRotate.targetRotation = leftGrabRotation;
+
+                    print("trying to grab");
+
+                    if (grabbedObject != null)
+                    {
+                        FixedJoint fj = grabbedObject.AddComponent<FixedJoint>();
+                        fj.connectedBody = rb;
+                        fj.breakForce = 9000;
+
+                        Vector3 newDirection = Vector3.RotateTowards(transform.forward, grabbedObject.transform.position - transform.position, Mathf.PI / 2, 0.0f);
+
+                        thisHand.targetRotation = Quaternion.LookRotation(newDirection);
+
+                        print(grabbedObject);
+                        print(fj);
+
+                        if (fj.connectedBody == rb)
+                        {
+                            leftAlreadyGrabbed = true;
+                            print("grabbed " + grabbedObject);
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    Destroy(grabbedObject.GetComponent<FixedJoint>());
+                    print("letting go");
+                    grabbedObject = null;
+
+                    armToRotate.targetRotation = naturalArmRotation;
+
+                    thisHand.targetRotation = naturalHandRotation;
+
+                    if (grabbedObject == null)
+                    {
+                        leftAlreadyGrabbed = false;
+                        print("dropped an object");
+                    }
+                }
             }
-
-
         }
         
 
@@ -47,7 +134,6 @@ public class Grab : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        print("ontriggerEnter");
         if (other.gameObject.CompareTag("Grabbable"))
         {
             grabbedObject = other.gameObject;
